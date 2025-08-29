1a2f5f6bc65b242c770187f9cb14d9b0dd53a01e
LamiIIIa <laminoji@gmail.com>
2025-08-29 18:15:44 +0300

    Добавлена функция рандомных чисел

--- src/cli.js
+++ src/cli.js
@@ -5,3 +5,8 @@ const name = readlineSync.question('May I have your name? ');
 
 console.log(`Hello, ${name}!`);
 }
+
+export const randomNumber = (min = 1, max = 100) => {
+    
+    return Math.floor(Math.random() * (max - 1 + min) + min);
+}
