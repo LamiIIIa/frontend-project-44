8ba971d2103b0fc53e00f34fd4419bc9260395ce
LamiIIIa <laminoji@gmail.com>
2025-09-04 13:02:44 +0300

    игра найди НОД добавлена!

---
+++ bin/brain-gcd.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import gameLaunch from '../src/index.js';
+import gcdGame from '../Games/gcd-game.js';
+
+gameLaunch(gcdGame);
---
+++ games/gcd-game.js
@@ -0,0 +1,30 @@
+import { yourAnswer } from '../src/cli.js';
+import {randomNumber} from '../src/helper.js';
+
+const gcdGame = {
+    description: 'Find the greatest common divisor of given numbers.',
+    getQuestion: () => {
+        const num1 = randomNumber();
+        const num2 = randomNumber();
+        return { num1, num2, text: `${num1} ${num2}`}
+    },
+    getAnswer: () => { 
+        return Number(yourAnswer());
+    },
+    getDecision: (question) => {
+        const { num1, num2 } = question;
+            let a = (num1 >= num2) ? num1 : num2;
+            let b = (num1 >= num2) ? num2 : num1;
+        while (b !== 0) {
+            const b2 = b;
+            b = a % b;
+            a = b2;
+            
+        }
+
+    return a;
+    
+    }
+}
+
+export default gcdGame;
