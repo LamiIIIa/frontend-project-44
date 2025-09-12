24fe3830ab9b8c9de214cfaf0ac8df02f30917bd
LamiIIIa <laminoji@gmail.com>
2025-09-12 11:03:52 +0300

    fixed brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import gameLaunch from '../src/index.js';
-import progGame from '../ames/progression-game.js';
+import progGame from '../games/progression-game.js';
 
 gameLaunch(progGame);
