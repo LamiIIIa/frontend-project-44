33f68cf017a0b02684c56cf0cb1598212063a728
LamiIIIa <laminoji@gmail.com>
2025-09-18 21:48:46 +0300

    fixed src/games

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import gameLaunch from '../src/index.js'
-import primeGame from '../games/prime-game.js'
+import gameLaunch from "../src/index.js";
+import primeGame from "../src/games/prime-game.js";
 
-gameLaunch(primeGame)
+gameLaunch(primeGame);
